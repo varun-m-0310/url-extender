bc0155368af538fe00395159ef163725e81c62f6
varun-m-0310 <75354452+varun-m-0310@users.noreply.github.com>
2022-12-05 13:04:45 +0530

    Moved env and app URLs to a separate config file (#2)

--- background.js
+++ background.js
@@ -1,25 +1,4 @@
-const ENV = {
-  IN: "https://incrm.cc.capillarytech.com/",
-  SG: "https://sgcrm.cc.capillarytech.com/",
-  EU: "https://eucrm.cc.capillarytech.com/",
-  TATA: "https://tatacrm.cc.capillarytech.com/",
-  Nightly: "https://crm-nightly-new.cc.capillarytech.com/",
-  Staging: "https://crm-staging-new.cc.capillarytech.com/",
-};
-
-const APPS = {
-  "Loyalty-New": "loyalty/ui/",
-  "Loyalty-Old": "loyaltyProgram/index",
-  "Loyalty-Promotions": "loyalty/ui/promotions/list",
-  "Loyalty-Bulk-Config": "loyalty/ui/bulk-configurations/list",
-  Campaigns: "campaigns/ui/list",
-  Coupons: "coupons/ui/",
-  Journeys: "journey/ui/",
-  "Audience-Manager": "audience-manager/list",
-  Creatives: "creatives/ui/v2",
-  "Insights+": "analytics/v2/",
-  Workbench: "businessProcesses/index",
-};
+import { ENV, APPS } from './config.js';
 
 const APP_KEY_LIST = Object.keys(APPS);
 const ENV_KEY_LIST = Object.keys(ENV);
---
+++ config.js
@@ -0,0 +1,23 @@
+export const ENV = {
+    IN: "https://incrm.cc.capillarytech.com/",
+    SG: "https://sgcrm.cc.capillarytech.com/",
+    EU: "https://eucrm.cc.capillarytech.com/",
+    TATA: "https://tatacrm.cc.capillarytech.com/",
+    Nightly: "https://crm-nightly-new.cc.capillarytech.com/",
+    Staging: "https://crm-staging-new.cc.capillarytech.com/",
+  };
+  
+export const APPS = {
+    "Loyalty-New": "loyalty/ui/",
+    "Loyalty-Old": "loyaltyProgram/index",
+    "Loyalty-Promotions": "loyalty/ui/promotions/list",
+    "Loyalty-Bulk-Config": "loyalty/ui/bulk-configurations/list",
+    Campaigns: "campaigns/ui/list",
+    Coupons: "coupons/ui/",
+    Journeys: "journey/ui/",
+    "Audience-Manager": "audience-manager/list",
+    Creatives: "creatives/ui/v2",
+    "Insights+": "analytics/v2/",
+    Workbench: "businessProcesses/index",
+  };
+  
