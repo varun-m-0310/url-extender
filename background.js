6d96cca9c30667dd7fc9eecb6e82b97af4b6c7ca
varun-m-0310 <75354452+varun-m-0310@users.noreply.github.com>
2022-12-06 16:16:52 +0530

    Added common cap cloud links (#4)

--- background.js
+++ background.js
@@ -1,43 +1,71 @@
-import { ENV, APPS } from './config.js';
+import {
+  PRODUCT_MAP,
+  ENV_PREFIX_MAP,
+  APPS,
+  URLGenerator,
+  SONAR_QUBE,
+} from "./config.js";
 
 const APP_KEY_LIST = Object.keys(APPS);
-const ENV_KEY_LIST = Object.keys(ENV);
+const ENV_KEY_LIST = Object.keys(ENV_PREFIX_MAP);
+const PRODUCT_KEY_LIST = Object.keys(PRODUCT_MAP);
+const SONAR_QUBE_LIST = Object.keys(SONAR_QUBE);
 
-const getFilteredList = (list, key) => list.filter(
-  (env) => env.toLowerCase().indexOf(key.toLowerCase()) !== -1
-);
+const getFilteredList = (list, key) =>
+  list.filter((env) => env.toLowerCase().indexOf(key.toLowerCase()) !== -1);
 
-const getSuggestionsAtom = (env, app) => {
+const getSuggestionsAtom = (env, product = "") => {
+  let URL = URLGenerator(env, product);
   return {
-    content: `${ENV[env]}${APPS[app]}`,
+    content: `${URL}`,
     deletable: true,
-    description: `${env}-${app} URL: <url>${ENV[env]}${APPS[app]}</url>`,
+    description: `${env}${product ? `-${product}` : ""} URL: <url>${URL}</url>`,
   };
 };
 
-const getSuggestions = (envText, appText) => {
+const getSuggestions = (envText, productText, appText) => {
+  const filteredProduct = getFilteredList(PRODUCT_KEY_LIST, productText);
   const filteredEnv = getFilteredList(ENV_KEY_LIST, envText);
   const filteredApp = getFilteredList(APP_KEY_LIST, appText);
+  const isSonarQube = getFilteredList(SONAR_QUBE_LIST, envText);
+
   const isKeyPresent = {};
   const suggestion = [];
-  if (filteredEnv.length && filteredApp.length) {
-    filteredEnv.forEach((env) => {
-      filteredApp.forEach((app) => {
-        if (isKeyPresent[`${env}-${app}`]) return;
-        isKeyPresent[`${env}-${app}`] = true;
-        suggestion.push(getSuggestionsAtom(env, app));
+
+  if (isSonarQube) {
+    suggestion.push({
+      content: `${SONAR_QUBE.SONAR_QUBE}`,
+      deletable: true,
+      description: `SonarQube URL: <url>${SONAR_QUBE.SONAR_QUBE}</url>`,
+    });
+  }
+
+  if (filteredProduct.length) {
+    filteredProduct.forEach((product) => {
+      ENV_KEY_LIST.forEach((env) => {
+        if (isKeyPresent[`${env}-${product}`]) return;
+        isKeyPresent[`${env}-${product}`] = true;
+        suggestion.push(getSuggestionsAtom(env, product));
       });
     });
   }
+
+  if (filteredEnv.length && !filteredProduct.length) {
+    filteredEnv.forEach((env) => {
+      suggestion.push(getSuggestionsAtom(env));
+    });
+  }
+
   if (filteredEnv.length) {
     filteredEnv.forEach((env) => {
-      APP_KEY_LIST.forEach((app) => {
-        if (isKeyPresent[`${env}-${app}`]) return;
-        isKeyPresent[`${env}-${app}`] = true;
-        suggestion.push(getSuggestionsAtom(env, app));
+      PRODUCT_KEY_LIST.forEach((product) => {
+        if (isKeyPresent[`${env}-${product}`]) return;
+        isKeyPresent[`${env}-${product}`] = true;
+        suggestion.push(getSuggestionsAtom(env, product));
       });
     });
   }
+
   if (filteredApp.length) {
     filteredApp.forEach((app) => {
       ENV_KEY_LIST.forEach((env) => {
@@ -47,11 +75,13 @@ const getSuggestions = (envText, appText) => {
       });
     });
   }
+
   return suggestion;
 };
 
 chrome.omnibox.setDefaultSuggestion({
-  description: "Enter {shortenURL} or {cluster}_{product_url} to open linked crm product",
+  description:
+    "Enter {shortenURL} or {cluster}_{product_url} to open linked crm product",
 });
 
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
@@ -59,12 +89,17 @@ chrome.omnibox.onInputChanged.addListener((text, suggest) => {
   const splitText = text.split("_");
   switch (splitText.length) {
     case 1: {
-      suggestion = getSuggestions(text, text);
+      suggestion = getSuggestions(text, text, text);
       break;
     }
     case 2: {
-      const [envText, appText] = splitText;
-      suggestion = getSuggestions(envText, appText);
+      const [envText, productText] = splitText;
+      suggestion = getSuggestions(envText, productText, envText);
+      break;
+    }
+    case 3: {
+      const [envText, productText, appText] = splitText;
+      suggestion = getSuggestions(envText, productText, appText);
       break;
     }
   }
@@ -79,7 +114,7 @@ chrome.omnibox.onInputEntered.addListener((text, OnInputEnteredDisposition) => {
 });
 
 /** SW gets deactivated due to inactivity, Workaround for the issue */
-chrome.alarms.create({ periodInMinutes: 4.9 })
+chrome.alarms.create({ periodInMinutes: 4.9 });
 chrome.alarms.onAlarm.addListener(() => {
-  console.log('log for debug')
+  console.log("log for debug");
 });
--- config.js
+++ config.js
@@ -1,23 +1,46 @@
-export const ENV = {
-    IN: "https://incrm.cc.capillarytech.com/",
-    SG: "https://sgcrm.cc.capillarytech.com/",
-    EU: "https://eucrm.cc.capillarytech.com/",
-    TATA: "https://tatacrm.cc.capillarytech.com/",
-    Nightly: "https://crm-nightly-new.cc.capillarytech.com/",
-    Staging: "https://crm-staging-new.cc.capillarytech.com/",
-  };
-  
 export const APPS = {
-    "Loyalty-New": "loyalty/ui/",
-    "Loyalty-Old": "loyaltyProgram/index",
-    "Loyalty-Promotions": "loyalty/ui/promotions/list",
-    "Loyalty-Bulk-Config": "loyalty/ui/bulk-configurations/list",
-    Campaigns: "campaigns/ui/list",
-    Coupons: "coupons/ui/",
-    Journeys: "journey/ui/",
-    "Audience-Manager": "audience-manager/list",
-    Creatives: "creatives/ui/v2",
-    "Insights+": "analytics/v2/",
-    Workbench: "businessProcesses/index",
-  };
-  
+  "Loyalty-New": "loyalty/ui/",
+  "Loyalty-Old": "loyaltyProgram/index",
+  "Loyalty-Promotions": "loyalty/ui/promotions/list",
+  "Loyalty-Bulk-Config": "loyalty/ui/bulk-configurations/list",
+  Campaigns: "campaigns/ui/list",
+  Coupons: "coupons/ui/",
+  Journeys: "journey/ui/",
+  "Audience-Manager": "audience-manager/list",
+  Creatives: "creatives/ui/v2",
+  "Insights+": "analytics/v2/",
+  Workbench: "businessProcesses/index",
+};
+
+export const SONAR_QUBE = {
+  "SONAR_QUBE": "https://sonarqube.cc-tooling.cctools.capillarytech.com/",
+}
+
+export const ENV_PREFIX_MAP = {
+  IN: "incrm",
+  SG: "sgcrm",
+  EU: "eucrm",
+  TATA: "tatacrm",
+  Nightly: "crm-nightly-new",
+  Staging: "crm-staging-new",
+  CN: "ningxia-crm",
+};
+
+const ENV_SUFFIX_MAP = {
+  CN: "cn.",
+};
+
+export const PRODUCT_MAP = {
+  WETTY: "wetty.",
+  RMQ: "rmq-intouch-direct.",
+  ROCKMONGO: "rockmongo.",
+  ELASTICHQ: "elastichq.",
+  GRAFANA: "grafana.",
+  KIBANA: "kibana.",
+  PMA: "rockmongo.",
+};
+
+export const URLGenerator = (env, product) =>
+  `https://${PRODUCT_MAP[product] || ""}${
+    ENV_PREFIX_MAP[env]
+  }.cctools.capillarytech.${ENV_SUFFIX_MAP[env] || ""}com/`;
